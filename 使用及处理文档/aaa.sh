4fb63803f6524b46c0c4bd2c342a70c52dbb93fe
mao_ying_ming <elwin_mao@163.com>
2014-10-20 17:26:24 +0800

    完善文档

--- 使用及处理文档/aaa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf /root/new/bbb.txt
-echo "start" > /root/new/bbb.txt
-date >> /root/new/bbb.txt
-/usr/local/runScript -r /root/Documents/test-ods.sikuli
-date >> /root/new/bbb.txt
-echo "stop" >> /root/new/bbb.txt
-lxterminal &
-#shutdown -h now
