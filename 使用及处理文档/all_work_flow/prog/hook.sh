9a5fe5dd41c7140a0e4bd156a5ee89b309308baf
root <root@ubuntu-xyong.(none)>
2014-11-05 10:01:50 +0800

    update hook.sh

--- 使用及处理文档/all_work_flow/prog/hook.sh
+++ 使用及处理文档/all_work_flow/prog/hook.sh
@@ -1,3 +1,7 @@
 #/bin/bash
-cd /mnt/freenas/all_work_flow/prog
+cd /home/myming/git_tijiao/all_work_flow/prog
+scp -r  root@124.16.141.184:/usr/local/share/cg-rtl/testcase/testcase2/*  ./test_case
+ssh root@124.16.141.184 "rm -rf /usr/local/share/cg-rtl/testcase/testcase2/*"
 ./start_all.sh
+scp -r ./test_result/*  root@124.16.141.184:/usr/local/apache-tomcat-7.0.56/webapps/ROOT/mym_test
+rm -rf ./test_result/*
